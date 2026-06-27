b78497b1518ec3a4aa387aa5767901e35e3d4346
Kitty <4lessandro@proton.me>
2026-06-27 20:26:58 +0000

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 
 // Start thingies for the calculator
 const char *expr;
-
 void skip_spaces(){
 	while (isspace(*expr))
 		expr++;
@@ -85,9 +84,10 @@ int main(int argc, char *argv[]){
 		printf("Swiss Army Knife\n");
 		printf("Multiple mini-programs in one\n\n");
 		printf("Avaidable tools:\n");
-		printf("btd	    Binary to decimal converter\n");
+		printf("btd	Binary to decimal converter\n");
 		printf("calc    Calculator\n");
-		printf("dtb	    Decimal to binary converter\n");
+		printf("dtb	Decimal to binary converter\n");
+		printf("fcalc   Factorial calculator\n");
 		printf("unixt	Current Unix time\n");
 		printf("unixtc	Unix time to date converter\n");
 		return 0;
@@ -176,6 +176,29 @@ int main(int argc, char *argv[]){
 		}
 		printf("%g\n", result);
 		return 0;
+	}
+	// FACTORIAL CALCULATOR (fcalc)
+	if(strcmp(argv[1], "fcalc") == 0){
+		if(argc != 3){
+			printf("Error: Use %s fcalc <number>\n", argv[0]);
+			return 1;
+		}
+		int number = atoi(argv[2]);
+		if(number < 0){
+			printf("ERROR: Factorial is not defined by negative numbers\n");
+			return 1;
+		}
+		if(number > 20){
+			printf("ERROR: Result is too large for the 'unsigned long long' variable\n");
+			return 1;
+		}
+		unsigned long long result = 1;
+		for(int i = 2; i <= number; i++){
+			result *= i;
+		}
+		printf("%llu\n", result);
+		return 0;
+
 	}
 	printf("ERROR: Unknown argument: %s\n", argv[1]);
 	return 1;
